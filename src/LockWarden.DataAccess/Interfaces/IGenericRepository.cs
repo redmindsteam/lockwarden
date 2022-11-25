fb86f18354bbb4539e7b2ef7793fbd1603e3d7c9
fayziyev <bidamfayz007@gmail.com>
2022-11-25 11:30:32 +0500

    DataAccess created user repository

---
+++ src/LockWarden.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using LockWarden.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockWarden.DataAccess.Interfaces
+{
+    public interface IGenericRepository<T>
+    {
+        public Task<bool> CreateAsync(T entity);
+
+        public Task<bool> DeleteAsync(int id);
+        public Task<bool> UpdateAsync(int id,User user);
+
+        public Task<User> GetUserAsync(int id);
+        public Task<List<User>> GetAllUsers();
+    }
+}
--- src/LockWarden.Domain/Models/User.cs
+++ src/LockWarden.Domain/Models/User.cs
@@ -5,14 +5,14 @@ public class User : IReferenceable
 	public int Id { get; set; }
 	public string Name { get; set; }
 	public string Login { get; set; }
-	public string Password { get; set; }
+	public string PasswordHash { get; set; }
 	public string Salt { get; set; }
 
 	public User(string name, string login, string password, string salt)
 	{
 		Name = name;
 		Login = login;
-		Password = password;
+		PasswordHash = password;
 		Salt = salt;
 	}
 }
