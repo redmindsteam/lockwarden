536469fe9ae0eae37123a3b20785ee3e559ac390
Shohrux Zokirjonov <zokirjonovshohruxbek1016@gmail.com>
2022-11-29 21:30:32 +0500

    Full Login page added closing/minimised buttons rebuilt Login/regs buttons

--- src/LockWarden.Desktop/Windows/LoginWindow.xaml.cs
+++ src/LockWarden.Desktop/Windows/LoginWindow.xaml.cs
@@ -23,5 +23,47 @@ namespace LockWarden.Desktop.Windows
         {
             InitializeComponent();
         }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtemail.Focus();
+        }
+
+        private void txtemail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtemail.Text) && txtemail.Text.Length > 0) textEmail.Visibility = Visibility.Collapsed;
+            else textEmail.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPasswords.Focus();
+        }
+
+        private void txtPasswords_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPasswords.Password) && txtPasswords.Password.Length > 0) textPassword.Visibility = Visibility.Collapsed;
+            else textPassword.Visibility = Visibility.Visible;
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Border_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
