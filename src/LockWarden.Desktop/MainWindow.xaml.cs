75dd1b4e65d9931baff9812ff177e548d62453d1
Shohrux Zokirjonov <zokirjonovshohruxbek1016@gmail.com>
2022-11-29 11:33:05 +0500

    Updatetemprory

--- src/LockWarden.Desktop/MainWindow.xaml.cs
+++ src/LockWarden.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 
 namespace LockWarden.Desktop
 {
@@ -11,5 +12,39 @@ namespace LockWarden.Desktop
 		{
 			InitializeComponent();
 		}
+
+		private void textEmail_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			txtEmail.Focus();
+		}
+
+		private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			if(!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+			{
+				txtEmail.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				txtEmail.Visibility = Visibility.Visible;
+			}
+		}
+
+		private void textPassword_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			txtPasswords.Focus();
+		}
+
+		private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+            if (!string.IsNullOrEmpty(txtPasswords.Password) && txtPasswords.Password.Length > 0)
+            {
+                txtPasswords.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                txtPasswords.Visibility = Visibility.Visible;
+            }
+        }
 	}
 }
