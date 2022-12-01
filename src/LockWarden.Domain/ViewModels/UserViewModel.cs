018a625627b8603f8fe7b0df7e0f77ec791dc7ed
fayziyev <bidamfayz007@gmail.com>
2022-12-01 18:11:33 +0500

    bir nima

---
+++ src/LockWarden.Domain/ViewModels/UserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockWarden.Domain.ViewModels
+{
+    internal class UserViewModel
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Login { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public UserViewModel(string name, string login, string password)
+        {
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+    }
+}
