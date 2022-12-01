cecad736d866d36dc936a0fcac0063b9eca360f7
Kkkiittt <khamidov357@gmail.com>
2022-12-01 17:32:03 +0500

    Added Crypter and Helper services

---
+++ src/LockWarden.Service/Tools/Crypter.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace LockWarden.Service.Tools;
+
+public class Crypter
+{
+	public static string Ciphr(string s, int seed, Crypt operand)
+	{
+		Random rndm = new Random(seed);
+		StringBuilder sb = new StringBuilder();
+		foreach(char c in s)
+		{
+			sb.Append((char)(c + (int)operand * rndm.Next(-100, 100)));
+		}
+		return sb.ToString();
+	}
+	public static (string hash, string salt) Hash(string pasw)
+	{
+		string salt = Guid.NewGuid().ToString();
+		return (ComputeHash(pasw, salt), salt);
+	}
+	private static string ComputeHash(string pasw, string salt)
+	{
+		pasw += salt;
+		byte[] inBytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(pasw));
+		string hashed = Helper.ArrayToString(inBytes);
+		return hashed;
+	}
+	public static bool Verify(string correct, string check, string salt)
+	{
+		string hashedCheck = ComputeHash(check, salt);
+		return correct == hashedCheck;
+	}
+}
+public enum Crypt
+{
+	Encrypt = 1,
+	Decrypt = -1,
+}
---
+++ src/LockWarden.Service/Tools/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace LockWarden.Service.Tools;
+
+public class Helper
+{
+	public static string ArrayToString(byte[] arr)
+	{
+		StringBuilder res = new StringBuilder();
+		for(int i = 0; i < arr.Length; i++)
+		{
+			res.Append($"{arr[i]:X2}");
+		}
+		return res.ToString().ToLower();
+	}
+	public static int ToSeed(string passw)
+	{
+		byte[] bytes = Encoding.UTF8.GetBytes(passw);
+		int result = BitConverter.ToInt32(SHA256.Create().ComputeHash(bytes));
+		return result;
+	}
+	public static string ImageToString(string path)
+	{
+		var img = File.ReadAllBytes(path);
+		return Convert.ToBase64String(img);
+	}
+	public static void StringToImage(string str, string path)
+	{
+		var img = Convert.FromBase64String(str);
+		File.WriteAllBytes(path, img);
+	}
+}
