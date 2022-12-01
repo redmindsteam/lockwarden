314f2d119a3d6fc414ab7364e3f7312b7001e278
Azamjon <azamjondev@gmail.com>
2022-12-01 11:53:49 +0500

    dashboard

--- src/LockWarden.Desktop/MainWindow.xaml.cs
+++ src/LockWarden.Desktop/MainWindow.xaml.cs
@@ -12,5 +12,23 @@ namespace LockWarden.Desktop
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
 }
