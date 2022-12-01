847b8372a3c64a0cc43b62daaa4a8002fb7fdffa
fayziyev <bidamfayz007@gmail.com>
2022-12-01 13:03:47 +0500

    FullRepository yaratildi u bilan boshqa repositorylarga oson murojaat qilish mumkun. Database to'liq yozilgandan keyin qolgan qismini qo'shaman

---
+++ src/LockWarden.DataAccess/Repositories/FullRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockWarden.DataAccess.Repositories
+{
+    public static class FullRepository
+    {
+        public static UserRepository UserRepository { get; set; }
+
+    }
+}
