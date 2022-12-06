03fb9d360edd3a1e8b39f7e932b81e309c5870ad
fayziyev <bidamfayz007@gmail.com>
2022-12-07 01:53:49 +0500

    Created ViewModels

---
+++ src/LockWarden.Domain/ViewModels/CardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockWarden.Domain.ViewModels
+{
+    public class CardViewModel
+    {
+        public string Bank { get; set; }
+        public string Number { get; set; }
+        public string Pin { get; set; }
+        public string Name { get; set; }
+
+        public CardViewModel(string bank, string number, string pin, string name)
+        {
+            Bank = bank;
+            Number = number;
+            Pin = pin;
+            Name = name;
+        }
+    }
+}
